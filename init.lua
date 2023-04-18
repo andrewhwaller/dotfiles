746db47feaa2daaba2158cf44a10d5185293f1bc
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-04-18 02:38:12 -0500

    Update harpoon bindings

--- init.lua
+++ init.lua
@@ -274,7 +274,7 @@ vim.keymap.set('n', '}', '}zz')
 vim.keymap.set('n', '<leader>f', ':lua require("harpoon.ui").toggle_quick_menu()<CR>',
   { silent = true, noremap = true }
 )
-vim.keymap.set('n', '<leader>a', ':lua require("harpoon.mark").add_file()<CR>',
+vim.keymap.set('n', '<leader>m', ':lua require("harpoon.mark").add_file()<CR>',
   { silent = true, noremap = true }
 )
 vim.keymap.set('n', '<leader>h', ':lua require("harpoon.ui").nav_file(1)<CR>',
