c9c084186232a617fb514cc6fab0b9263d8afe63
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-09-24 17:45:38 -0500

    Update spacing, re-add true zen, and fix vim global

--- init.lua
+++ init.lua
@@ -20,7 +20,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require('lazy').setup({
-  { 'j-hui/fidget.nvim', tag = 'legacy' },
+  {
+    'j-hui/fidget.nvim',
+    tag = 'legacy'
+  },
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -52,7 +55,9 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
+      pcall(require('nvim-treesitter.install').update {
+        with_sync = true
+      })
     end,
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
@@ -61,7 +66,9 @@ require('lazy').setup({
   -- Highlight function args
   {
     'm-demare/hlargs.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' }
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter'
+    }
   },
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -83,7 +90,10 @@ require('lazy').setup({
   'ThePrimeagen/git-worktree.nvim',
   'numToStr/FTerm.nvim',
   'stevearc/dressing.nvim',
-  { 'catppuccin/nvim', name = 'catppuccin' },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin'
+  },
   'nvim-lualine/lualine.nvim',           -- Fancier statusline
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
   'numToStr/Comment.nvim',               -- "gc" to comment visual regions/lines
@@ -106,19 +116,19 @@ require('lazy').setup({
     end
   },
   -- Fuzzy Finder (files, lsp, etc)
-  { 
+  {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 
+    dependencies = {
       'nvim-lua/plenary.nvim'
-    } 
+    }
   },
   'ThePrimeagen/harpoon',
   -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
   {
     'nvim-telescope/telescope-fzf-native.nvim',
     build = 'make',
-    cond = vim.fn.executable 'make' == 1 
+    cond = vim.fn.executable 'make' == 1
   },
   'tpope/vim-endwise',
   'tpope/vim-surround',
@@ -129,6 +139,12 @@ require('lazy').setup({
     end
   },
   'famiu/nvim-reload',
+  {
+    'Pocco81/true-zen.nvim',
+    config = function()
+      require('true-zen').setup {}
+    end
+  }
 })
 
 require('nvim-treesitter.configs').setup {
@@ -489,8 +505,17 @@ local servers = {
   tsserver = {},
   lua_ls = {
     Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+      workspace = {
+        checkThirdParty = false
+      },
+      telemetry = {
+        enable = false
+      },
+      diagnostics = {
+        globals = {
+          'vim'
+        }
+      },
     },
   },
 }
