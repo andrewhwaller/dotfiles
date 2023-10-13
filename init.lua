4f120e776bc55556a490401023c9b539e22f12f1
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-10-13 17:49:02 -0500

    Update ensure_installed

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'ruby', 'rust', 'javascript', 'typescript', 'tsx', 'vimdoc', 'vim', 'query', 'latex', 'lua', 'bash' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'ruby', 'rust', 'javascript', 'typescript', 'tsx', 'vimdoc', 'vim', 'query', 'bash' },
   autotag = { enable = true },
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
