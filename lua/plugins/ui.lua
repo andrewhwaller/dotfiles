7c5b95714ae351f23b007db0a856cbd22c424b1e
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-10-28 11:20:46 -0500

    Fix quotes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -23,7 +23,7 @@ return {
     name = 'catppuccin'
   },
   'nvim-lualine/lualine.nvim',           -- Fancier statusline
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
   'numToStr/Comment.nvim',               -- "gc" to comment visual regions/lines
   'tpope/vim-sleuth',                    -- Detect tabstop and shiftwidth automatically
   'tpope/vim-dadbod',
