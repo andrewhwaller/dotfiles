8375c730519ae78f79d1a8e3e67fbc86820cf2bf
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-12-23 12:01:44 -0600

    Add lspkind

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,6 +14,9 @@ return {
     }
   },
   {
+    'onsails/lspkind.nvim',
+  },
+  {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
