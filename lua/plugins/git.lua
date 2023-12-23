7997cb8254befc7708c9a9616d8d86da08d6a816
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2023-12-23 12:01:53 -0600

    copilot mang

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'lewis6991/gitsigns.nvim',
-  -- 'github/copilot.vim',
+  'github/copilot.vim',
   'kdheepak/lazygit.nvim',
   'ThePrimeagen/git-worktree.nvim'
 }
