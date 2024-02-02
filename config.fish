facdffbfae5309a92bf107734275446906bf0e5e
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-02-02 12:54:05 -0600

    Update config.fish

--- config.fish
+++ config.fish
@@ -16,7 +16,25 @@ alias lg="lazygit"
 fzf_configure_bindings --directory=\cf --variables=\e\cv
 
 set -U fish_greeting
+set -x DISABLE_SPRING 1
 
 . (rbenv init - | source)
+
+function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  if test -f .nvmrc
+    set node_version (node -v)
+    set nvmrc_node_version (nvm list | grep (cat .nvmrc))
+
+    if set -q $nvmrc_node_version
+      nvm install
+    else if string match -q -- "*$node_version" $nvmrc_node_version
+      # already current node version
+    else
+      nvm use
+    end
+  end
+end
+
+__check_nvm
 
 starship init fish | source
