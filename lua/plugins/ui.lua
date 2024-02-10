51148962c08ae9d141caa0dcb56928de7b966dfe
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-02-09 18:01:29 -0600

    Remove chatgpt plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,17 +5,6 @@ return {
       'nvim-treesitter/nvim-treesitter'
     }
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  },
   'numToStr/FTerm.nvim',
   'stevearc/dressing.nvim',
   {
