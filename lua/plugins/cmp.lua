81a443f52baf927acc1fcf06f184ac5daeef39cf
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-03-02 13:11:41 -0600

    Update cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,26 +4,21 @@ return {
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-omni',
-      'L3MON4D3/LuaSnip',
+      'onsails/lspkind-nvim',
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+      },
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
       'jalvesaq/cmp-zotcite',
+      'micangl/cmp-vimtex',
       'hrsh7th/cmp-nvim-lsp-signature-help'
     }
   },
-  {
-    'onsails/lspkind.nvim',
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp"
-  },
-  {
-    'vim-scripts/dbext.vim'
-  }
 }
