249ff6047a8bd075ab1adf2b9cb3ce65a2c5caf3
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-06-01 18:37:44 -0500

    Update git file

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,7 @@
 return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
-  'lewis6991/gitsigns.nvim',
   'github/copilot.vim',
   'kdheepak/lazygit.nvim',
-  'ThePrimeagen/git-worktree.nvim',
-  'f-person/git-blame.nvim',
+  'ThePrimeagen/git-worktree.nvim'
 }
