40bc8fa98cc364f6bc0fd5db373157144713bcf2
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-06-01 18:38:35 -0500

    Update telescope and treesitter configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,22 @@ return {
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim'
-    }
+    },
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
+          },
+        },
+      }
+      telescope.load_extension('git_worktree')
+      pcall(telescope.load_extension, 'fzf')
+    end
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
