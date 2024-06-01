8f2650b45341c3b87260e4e0f918de40ff3ade23
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-06-01 18:37:12 -0500

    Move keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -63,3 +63,57 @@ vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
 vim.keymap.set("n", "<leader>e", ":Lexplore %:p:h<CR>",
   { silent = true, noremap = true }
 )
+
+local harpoon = require("harpoon")
+-- Harpoon keymaps
+vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>f", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>k", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>l", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+local telescope_builtin = require('telescope.builtin')
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', telescope_builtin.current_buffer_fuzzy_find, { desc = '[ ] Find in current buffer' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  telescope_builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+vim.keymap.set("n", "<leader>sa", ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", {})
+
+vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>", {})
+vim.keymap.set("n", "<leader>gg", ":GitBlameOpenCommitURL<CR>", {})
+
+-- Netrw keymaps
+vim.keymap.set("n", "<leader>ee", ":Explore %:p:h<CR>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>ev", ":Vexplore %:p:h<CR>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>es", ":Sexplore %:p:h<CR>",
+  { silent = true, noremap = true }
+)
