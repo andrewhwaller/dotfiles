b8d35c5e42ab626c1b10c23073958ae22c4ce477
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-06-01 18:38:24 -0500

    Add files for mason and mason-lspconfig

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,87 @@
+return {
+  'williamboman/mason-lspconfig.nvim',
+  config = function()
+    local mason_lspconfig = require('mason-lspconfig')
+    local servers = {
+      rust_analyzer = {},
+      ruby_lsp = {},
+      tsserver = {},
+      cssls = {
+        css = {
+          validate = true,
+          lint = {
+            unknownAtRules = 'ignore'
+          }
+        },
+        less = {
+          validate = true,
+          lint = {
+            unknownAtRules = 'ignore'
+          }
+        },
+        scss = {
+          validate = true
+        },
+      },
+      lua_ls = {
+        Lua = {
+          workspace = {
+            checkThirdParty = false
+          },
+          telemetry = {
+            enable = false
+          },
+          diagnostics = {
+            globals = {
+              'vim'
+            }
+          },
+        },
+      },
+    }
+
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(servers)
+    })
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+    local on_attach = function(_, bufnr)
+      -- NOTE: Remember that lua is a real programming language, and as such it is possible
+      -- to define small helper and utility functions so you don't have to repeat yourself
+      -- many times.
+      --
+      -- In this case, we create a function that lets us more easily define mappings specific
+      -- for LSP related items. It sets the mode, buffer and description for us each time.
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+      end
+
+      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+      nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+      end, { desc = 'Format current buffer with LSP' })
+    end
+
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        require('lspconfig')[server_name].setup {
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = servers[server_name],
+        }
+      end,
+    }
+  end
+}
