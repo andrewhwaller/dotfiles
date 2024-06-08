af935532abf35ffc22e46a9721d7650ae473e94b
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-06-08 17:02:26 -0500

    Remove unneeded aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,6 @@ end
 
 alias riptear="/usr/bin/open -a '/Applications/Brave Browser.app' 'https://www.youtube.com/watch?v=KO-2rDf3SXg&t=1s'"
 alias exo="exa --color=auto --icons --long -h -a --git --no-permissions --no-user --time=accessed --group-directories-first"
-alias cht="~/dotfiles/cht.sh"
-alias dbui="nvim -c 'DBUI'"
-alias dsh="gh dash"
-alias vim="nvim"
-alias vi="nvim"
 alias v="nvim"
 alias lg="lazygit"
 
