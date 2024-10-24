e7bf5654cbf4e04763ee4b394aa9f57e7122ad47
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-10-24 17:25:31 -0500

    Update vimtex config

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,5 +2,10 @@ return {
   'lervag/vimtex',
   config = function()
     vim.g.vimtex_view_method = 'zathura'
+
+    local os_name = vim.loop.os_uname().sysname
+    if os_name == 'Darwin' then
+      vim.g.vimtex_view_method = 'skim'
+    end
   end
 }
