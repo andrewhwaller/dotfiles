bf7e830977f9cafedb8775f8c14a7a83ff18805a
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2024-10-24 22:17:27 -0500

    Update vimtex config

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,18 +1,26 @@
 return {
   'lervag/vimtex',
   config = function()
-    vim.g.vimtex_view_method = 'zathura'
-
     local os_name = vim.loop.os_uname().sysname
-    if os_name == 'Darwin' then
-      vim.g.vimtex_view_method = 'skim'
-    end
 
-    vim.api.nvim_create_autocmd({"CursorMoved", "BufWritePost"}, {
-      pattern = "*.tex",
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VimtexEventCompileSuccess",
       callback = function()
         vim.cmd("VimtexView")
       end,
     })
+
+    if os_name == 'Darwin' then
+      vim.g.vimtex_view_method = 'skim'
+    elseif os_name == 'Linux' then
+      vim.g.vimtex_view_method = 'zathura'
+
+      vim.api.nvim_create_autocmd("CursorMoved", {
+        pattern = "*.tex",
+        callback = function()
+          vim.cmd("VimtexView")
+        end,
+      })
+    end
   end
 }
