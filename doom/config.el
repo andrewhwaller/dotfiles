c87ebe8a2236b8f5c79a4242b66b473dd2b00d09
Andrew Waller <andrew@andrewhwaller.com>
2025-02-14 15:18:09 -0600

    Update config.el

--- doom/config.el
+++ doom/config.el
@@ -66,7 +66,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/github/org")
+(before! org
+  (setq org-directory "~/github/org"))
 
 (after! org
   (setq org-deadline-warning-days 4
