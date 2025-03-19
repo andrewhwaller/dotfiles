8aba5f67dafecbe0d96b360138e0747642dc651a
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-03-18 20:09:29 -0500

    Update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,6 +8,12 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Window navigation keymaps
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window', silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to lower window', silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window', silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window', silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -43,8 +49,6 @@ function OpenFTermAtCurrentFileDirectory()
   terminal:toggle()
 end
 vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
-vim.keymap.set('n', '<leader>t', ':lua OpenFTermAtCurrentFileDirectory()<CR>', { silent = true })
-vim.keymap.set('t', '<ESC>', require('FTerm').close, { silent = true })
 
 -- lazygit.nvim
 vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { silent = true, noremap = true })
