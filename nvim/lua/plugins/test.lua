1a7d512c963affdc73758c68ed1814ec38383862
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-03-18 20:09:41 -0500

    Add keymaps for vim-test

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,5 +1,13 @@
 return {
   {
-    'vim-test/vim-test'
+    'vim-test/vim-test',
+    config = function()
+      vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', { desc = 'Run test nearest', silent = true })
+      vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = 'Run test file', silent = true })
+      vim.keymap.set('n', '<leader>T', ':TestSuite<CR>', { desc = 'Run test suite', silent = true })
+      vim.keymap.set('n', '<leader>tt', ':TestClass<CR>', { desc = 'Run test last', silent = true })
+      vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = 'Run test last', silent = true })
+      vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>', { desc = 'Visit test file', silent = true })
+    end
   }
 }
