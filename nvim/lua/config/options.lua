a698c61d5c0a03085580bd7e7eb96338d19c3c2c
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-03-29 14:05:05 -0500

    Update diagnostic config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -72,3 +72,14 @@ vim.filetype.add({
     ['.*%.blade%.php'] = 'blade',
   },
 })
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
