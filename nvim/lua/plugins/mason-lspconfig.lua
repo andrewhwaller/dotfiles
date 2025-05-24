7db79528144b2a3db3b1ffccf4fc89a35fcb0560
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-05-24 10:42:33 -0500

    Update mason lsp config

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -74,14 +74,17 @@ return {
       end, { desc = 'Format current buffer with LSP' })
     end
 
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-        }
-      end,
-    }
+    mason_lspconfig.setup({
+      ensure_installed = vim.tbl_keys(servers),
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+          }
+        end,
+      }
+    })
   end
 }
