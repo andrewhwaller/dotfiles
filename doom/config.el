39276b8a93e4a24eef4c1124038839911ec39eef
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-05-24 10:42:28 -0500

    Update doom config

--- doom/config.el
+++ doom/config.el
@@ -38,8 +38,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "BerkeleyMono Nerd Font Mono" :size 14 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "BerkeleyMono Nerd Font Mono" :size 14))
+(setq doom-font (font-spec :family "TX-02" :size 14 :weight 'semi-light)
+     doom-variable-pitch-font (font-spec :family "TX-02" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
