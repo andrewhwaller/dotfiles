07f479313c2cd2a0097210733d5bc95c68086616
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-03 14:02:26 -0500

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,8 @@ set -U fish_user_paths $HOME/.config/composer/vendor/bin $fish_user_paths
 set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 set -U fish_user_paths /opt/homebrew/sbin $fish_user_paths
 
+~/.local/bin/mise activate fish | source
+
 starship init fish | source
 
 # Added by Windsurf
