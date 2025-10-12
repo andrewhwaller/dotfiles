c5637db72eb378311feac865004a61947009219d
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-11 20:23:40 -0500

    Update cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -73,7 +73,10 @@ return {
               return require("sidekick").nes_jump_or_apply()
             end,
             function() -- if you are using Neovim's native inline completions
-              return vim.lsp.inline_completion.get()
+              if vim.lsp.inline_completion then
+                return vim.lsp.inline_completion.get()
+              end
+              return false
             end,
             "fallback",
           },
