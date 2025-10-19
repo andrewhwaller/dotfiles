db232a141e9b5d5707763a1d38d89bbdb816969f
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-18 20:15:46 -0500

    Add auto-dark-mode

---
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,16 @@
+-- Auto dark mode plugin for macOS
+-- Automatically switches between light and dark themes based on system appearance
+return {
+  'f-person/auto-dark-mode.nvim',
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option_value('background', 'dark', {})
+      vim.cmd.colorscheme('catppuccin')
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option_value('background', 'light', {})
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+}
