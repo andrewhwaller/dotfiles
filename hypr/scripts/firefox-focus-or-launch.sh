7e5635e06252742c28f2c7d512d0206b5ee14127
Andrew H. Waller <andrew@andrewhwaller.com>
2025-11-13 12:13:12 -0600

    Add ff script

---
+++ hypr/scripts/firefox-focus-or-launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Open new Firefox window from existing instance or launch if not running
+
+# This will create a new window from the existing Firefox instance if running,
+# or launch Firefox if it's not running
+firefox --new-window &
